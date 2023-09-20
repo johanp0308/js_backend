cc84c6cba251681f8ecf03dd6421f40af5586202
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-09-20 07:46:57 -0500

    feat: :sparkles: Terminando aun no
    @johanp0308

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 import crud from "./modules/dataCRUD.js"
 
-let myform = document.querySelector("form");
-let dTable = document.querySelector("#data-table");
+const myform = document.querySelector("form");
+const dTable = document.querySelector("#data-table");
+
+
+const crearBtns=()=>{
+    btnsDele = document.querySelectorAll(".btn-del");
+    btnsEdit = document.querySelectorAll(".btn-edit");
+}
+
+const asignarEvents=(element)=>{
+    element.forEach((btn)=>{
+        btn.addEventListener("click",(btn)=>{
+            console.log(btn)
+        })
+    })
+}
 
 myform.addEventListener("submit",(e) => {
     e.preventDefault();
     crud.createData(e);
 });
 
-
-addEventListener("DOMContentLoaded",(e)=>{
+await addEventListener("DOMContentLoaded",(e)=>{
     e.preventDefault();
+    let butons = btnsDele;
     crud.actulizarTable(dTable);
 });
+
+const btnsDele = 
