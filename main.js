cc6b2a2e99ab1f491df10ed4f6ddb0671e182416
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-09-19 23:38:39 -0500

    feat: :sparkles: functionalities were created: Create and update
    functionalities were created to create and load data to the database, in addition to updating the table in the html
    
    @johanp0308

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+import crud from "./modules/dataCRUD.js"
 
 let myform = document.querySelector("form");
+let dTable = document.querySelector("#data-table");
 
 myform.addEventListener("submit",(e) => {
     e.preventDefault();
-    
+    crud.createData(e);
 });
 
+
+addEventListener("DOMContentLoaded",(e)=>{
+    e.preventDefault();
+    crud.actulizarTable(dTable);
+});
--- modules/dataCRUD.js
+++ modules/dataCRUD.js
@@ -1,11 +1,30 @@
-const create =  async(e)=>{
+
+const bdApi = "https://6509d04cf6553137159c10aa.mockapi.io/tabla"
+
+const createData =  async(e)=>{
     let data = Object.fromEntries(new FormData(e.target));
     data.valor = (typeof data.valor === "string") ? Number(data.valor) : null;
     let config ={
         method: "POST",
-        header: {"content-type":"application/json"},
+        headers: {"content-type":"application/json"},
         body: JSON.stringify(data)
     };
-    let res = await(await fetch("https://6509d04cf6553137159c10aa.mockapi.io/tabla",config)).json();
+    console.log(data);
+    let res = await(await fetch(bdApi,config)).json();
+}
 
+const deleteData = async(e)=>{
+
+}
+
+const actulizarTable = async(tabla)=>{
+    let res = await(await fetch(bdApi)).json();
+    res.map((e)=>{
+        tabla.insertAdjacementHTML("beforeend",`<tr> <td>${res.id}</td><td>${res.valor}</td><td>${res.caja}</td> </tr>`);
+    });
+}
+
+export default{
+    actulizarTable,
+    createData,
 }
